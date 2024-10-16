bc82d3b2474c0e9330c7a653efaceaa5b09b1375
RZX33 <s24794@pjwstk.edu.pl>
2024-10-16 09:10:52 +0200

    Corrected dataset

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('cars.csv')
-
-df = df.drop(columns=['feature_0',
-                      'feature_1',
-                      'feature_2',
-                      'feature_3',
-                      'feature_4',
-                      'feature_5',
-                      'feature_6',
-                      'feature_7',
-                      'feature_8',
-                      'feature_9',
-                      'location_region',
-                      'is_exchangeable'])
-
-df.loc[df["up_counter"] <= 1, "up_counter"] = 1
-df.loc[df["up_counter"] > 1, "up_counter"] = 0
+df = pd.read_csv('cars.csv', delimiter=';')
 
 print(df.head())
