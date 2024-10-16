ecde413d64b58f69dd9a480dfd3beeb71972932c
RZX33 <s24794@pjwstk.edu.pl>
2024-10-16 09:33:14 +0200

    Implemented reading from file

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pd
+#import autogluon
+import pycaret
+
+pd.set_option('display.max_columns', 100)
 
 df = pd.read_csv('cars.csv', delimiter=';')
+df.drop(columns='Unnamed: 17', inplace=True)
+
 
 print(df.head())
+print(df.shape)
+print(df.describe())
+print(df.columns)
+print(df.dtypes)
+print(df.isnull().sum())
